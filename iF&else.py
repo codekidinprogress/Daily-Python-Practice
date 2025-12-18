c9ec5693f570bbaa24577ab6185feecd760a7551
Dev <codekidinprogress@gmail.com>
2025-12-18 22:52:58 +0530

    Add

---
+++ iF&else.py
@@ -0,0 +1,9 @@
+temperature = int(input("Enter The Temperature: "))
+if temperature > 30:
+    print("It's a Hot Day!")
+elif temperature > 20:
+    print("It's a Nice day!")
+elif temperature > 10:
+    print("It's a bit Cold!")
+else:
+    print("Not Found")
