6ff54f869c5de9b2f5d09e4d13097909e7b3de5c
Dev <codekidinprogress@gmail.com>
2025-12-18 22:38:57 +0530

    Add Addition program

---
+++ Addition.py
@@ -0,0 +1,4 @@
+input1 = input("Enter First Number: ")
+input2 = input("Enter Second Number: ")
+Addition = int(input1) + int(input2)
+print("The Addition of "+input1+ " and "+ input2+ " is "+ str(Addition))
